ad2ebe70cda47b77e7dda91d14bf343cb188fd0b
ze00ro <721908811@qq.com>
2024-03-27 19:11:24 +0800

    sandbox 的工作目录

--- interpreter/core/utils/get_user_info_string.py
+++ interpreter/core/utils/get_user_info_string.py
@@ -5,8 +5,8 @@ import platform
 
 def get_user_info_string():
     username = getpass.getuser()
-    current_working_directory = os.getcwd()
-    operating_system = platform.system()
+    # current_working_directory = os.getcwd()
+    # operating_system = platform.system()
     default_shell = os.environ.get("SHELL")
 
-    return f"[User Info]\nName: {username}\nCWD: {current_working_directory}\nSHELL: {default_shell}\nOS: {operating_system}"
+    return f"[User Info]\nName: {username}\nCWD: /home/user\nSHELL: {default_shell}\nOS: Linux"
